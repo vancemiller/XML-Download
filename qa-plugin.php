9d7b6e2648fec9eaa072e4c32f653214e0d8b9b1
Vance Barden Miller <vbmiller@onefish.cs.unc.edu>
2014-05-23 09:55:15 -0400

    initial commit

---
+++ qa-plugin.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+	Plugin Name: XML Q2A Site Download
+	Plugin URI:
+	Plugin Version: 0.5
+	Plugin Date: 2014-05-23
+	Plugin Author: Vance Miller
+	Plugin Aurhot URI:
+	Plugin License: MIT
+	Plugin Minimum Question2Answer Version 1.5
+	Plugin Update Check URI:
+*/
+
+if (!defined('QA_VERSION')) {
+	// don't allow this page to be requested directly from browser
+	header('Location: ../../');
+	exit;
+}
+
